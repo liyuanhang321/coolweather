16b58769e3e672742b346ece104bd6f3dc3b2eaf
liyuanhang321 <731026230@qq.com>
2018-02-08 10:05:10 +0800

    完成遍历省市县三级列表功能。

--- app/src/main/java/com/example/dell15/coolweather/db/Province.java
+++ app/src/main/java/com/example/dell15/coolweather/db/Province.java
@@ -13,19 +13,19 @@ public class Province extends DataSupport {
     public int getId(){
         return  id;
     }
-    public void setId(){
+    public void setId(int id){
         this.id = id;
     }
     public String getProvinceName(){
         return provinceName;
     }
-    public void setProvinceName(){
+    public void setProvinceName(String provinceName){
         this.provinceName = provinceName;
     }
     public int getProvinceCode(){
         return provinceCode;
     }
-    public void setProvinceCode(){
+    public void setProvinceCode(int provinceCode){
         this.provinceCode = provinceCode;
     }
 }
